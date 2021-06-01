99d300e3f5a9a8ca43b2900f13d94d13e1b42a22
gautamtiwari007 <gautamtiwari142@gmail.com>
2021-06-01 17:51:46 +0530

    Codec type change

--- js/main.js
+++ js/main.js
@@ -58,10 +58,7 @@ function handleDataAvailable(event) {
 
 function getSupportedMimeTypes() {
     const possibleTypes = [
-        'video/webm;codecs=vp9,opus',
-        'video/webm;codecs=vp8,opus',
         'video/webm;codecs=h264,opus',
-        'video/mp4;codecs=h264,aac',
     ];
     return possibleTypes.filter(mimeType => {
         return MediaRecorder.isTypeSupported(mimeType);
